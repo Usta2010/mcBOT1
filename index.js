cbccee4bbe47c6d05c15e04049df47796534597f
Usta2010 <89610412+Usta2010@users.noreply.github.com>
2026-02-14 21:45:49 +0300

    Initialize bot with Express server and Mineflayer

---
+++ index.js
@@ -0,0 +1,27 @@
+const mineflayer = require('mineflayer');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot Aktif!'));
+app.listen(3000);
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'agalarmclise.falix.gg',
+    port: 26273,
+    username: 'Bot_Agalar',
+    // version kısmını sildik, böylece otomatik algılar
+  });
+
+  bot.on('spawn', () => {
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+    }, 10000);
+  });
+
+  bot.on('end', () => setTimeout(createBot, 5000));
+  bot.on('error', (err) => console.log('Hata oluştu, tekrar deneniyor...'));
+}
+
+createBot();
